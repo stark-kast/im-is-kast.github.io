2433abb28c264354132033a9a549b6f3c5c0f24b
Kast <jdandfox@gmail.com>
2025-02-23 11:36:04 +0300

    Update and rename index.html to index.php
    HTML --> PHP
    Навигационное меню было упразднено

--- index.php
+++ index.php
@@ -9,27 +9,7 @@
 </head>
 <body>
 
-<div class="menu">
-<table><tr>
-	<th><a href="https://im-is-kast.github.io/">Начало</a></th>
-	<th>
-	<div class="dropdown">
-		<button><a href="https://im-is-kast.github.io/worlds/heu/heu_index.html">Хэуврохиос</a></button>
-		<div class="dropdown-content">
-			<a href="https://im-is-kast.github.io/worlds/heu/heu_books.html" class="nav">Рассказы</a>
-			<a href="#" class="nav nolink">Скоро!</a>
-			<a href="#" class="nav nolink">Скоро!</a>
-		</div>
-	</div>
-	</th>
-	<th><a href="" class="nolink" title="Список статей">Список статей</a></th>
-</tr></table>
-	<marquee behavior="scroll" direction="left" bgcolor="#111111" vspace="10" class="marq">
-		<span>Мини-Новости 2.27!</span>
-		<span>Данная вики находится в разработке, поэтому выглядит пусто и коряво. Скоро будет добавленны новые страницы и улучшена видимость!</span>
-		<span>Все рассказы стали доступны!</span>
-	</marquee>
-</div>
+<?php include 'nav_menu.php'; ?>
 
 <div class="content">
 	<h1>Мир тебе, Странник!</h1>
